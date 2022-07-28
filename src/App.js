2dc80986fd87b015b486e596c3e9884845dceed9
Igor Ershov <privetie@yandex.ru>
2022-07-28 20:19:15 +0300

    товары добавлены на бэкэнд, добавлен зук useEffect и fetch для изъятия данных с бэка

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
+import { useState, useEffect } from "react";
 import "./App.scss";
 import Categories from "./components/Categories";
 import Header from "./components/Header";
 import Products from "./components/Products";
-import data from "./db.json";
+
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("https://62e2b9d23891dd9ba8eec3f3.mockapi.io/items")
+      .then((res) => res.json())
+      .then((json) => setItems(json));
+  }, []);
+
   return (
     <div className="mainWrapper">
       <div className="wrapper">
         <Header />
         <Categories />
         <div className="cards">
-          {data.map((item) => (
+          {items.map((item) => (
             <Products key={item.id} {...item} />
           ))}
         </div>
